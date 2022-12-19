d00c7a01ea79230f193b19453556ea2065578e32
Tisha <iamtisha22@gmail.com>
2022-12-19 19:40:12 +1100

    useEffect dependencies

--- src/Home.js
+++ src/Home.js
@@ -10,6 +10,8 @@ const Home = () => {
     ]);
     // use props to pass this data from this component to BogList-reuseable component
 
+    const [name, setName] = useState('Tisha');
+
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
@@ -17,8 +19,12 @@ const Home = () => {
 
     useEffect(() =>{
         console.log('use effect run');
-        console.log(blogs);
-    });
+        console.log(name);
+
+    }, [name]);
+
+    // [ ] => empty dependency array makes the func run at the initial render only
+
 
     // the anon func inside useEffect hook fires on every render, any state/data change triggers a rerender
     // changing state inside useEffect could end up to a continuous loop of renders
@@ -27,7 +33,8 @@ const Home = () => {
         <div className="home">
             <BlogList blogs={blogs} title={"All Blogs"} handleDelete = {handleDelete}/>  
             {/* propertyname and value */}
-
+            <h1>{name}</h1>
+            <button onClick={() => setName('-Tisha')}> Click here</button>
         </div>
     );
 }
