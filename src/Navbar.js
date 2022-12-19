269a181351bfc77550c2ad665a3e6cee7188b8ce
Tisha <iamtisha22@gmail.com>
2022-12-19 16:49:27 +1100

    added Navbar and Home components

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>The Dobby Blog</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                
+                <a href="/create">Create</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
