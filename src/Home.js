c4a2a8c3af029d6b4864411634fbfece31aa5ac6
Tisha <iamtisha22@gmail.com>
2022-12-19 17:52:40 +1100

    react states

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,21 @@
+import { useState } from 'react';
+
 const Home = () => {
-    const handleClick = (e) => {
-        console.log('hello, ninjas', e);
-        alert("Hey");
+    //let name = "mario"; // make this variable reactive
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi');
+        setAge('30');
     }
 
-    const handleClickAgain = (name,e) => {
-        alert("Hey "+ name);
-        console.log(e.target);
-    }
     return (  
         <div className="home">
             <h2>Home Page</h2>
+            <p>{name} is {age} years old.</p>
             <button onClick={handleClick}> Click me </button>
-            <button onClick={(e) => {
-                handleClickAgain('mario', e);
-            }}> Click me again</button>
+
         </div>
     );
 }
