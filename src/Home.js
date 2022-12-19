9de2fc66be51735a4b37e52f6cc9ec18a793e730
Tisha <iamtisha22@gmail.com>
2022-12-19 21:45:36 +1100

    added a resuable hook

--- src/Home.js
+++ src/Home.js
@@ -1,39 +1,14 @@
-import { useState , useEffect} from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [err, setErr] = useState(null);
-
-    useEffect(() =>{
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs')
-            .then(response => {
-                if(!response.ok){
-                    throw Error("Could not fetch the data");
-                    // invalid endpoint
-                }
-                return response.json();
-            })
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-                setIsPending(false);
-                
-            }).catch( error => {
-                setErr(error.message);
-                setIsPending(false);
-            });
-        }, 1000);
-        
-    }, []);
-
+    
+    const {data, isPending, err} = useFetch('http://localhost:8000/blogs');
     return (  
         <div className="home">
             {err && <div>{err}</div>}
             {isPending && <div> Loading ...</div>}
-            {blogs && <BlogList blogs={blogs} title={"All Blogs"}/>  }
+            {data && <BlogList blogs={data} title={"All Blogs"}/>  }
         </div>
     );
 }
