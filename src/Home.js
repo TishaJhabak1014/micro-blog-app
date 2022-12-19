3ef31aa6d055fed34a6ca37b0a8a93d632ff019d
Tisha <iamtisha22@gmail.com>
2022-12-19 17:37:05 +1100

    added click events demo

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello, ninjas', e);
+        alert("Hey");
+    }
+
+    const handleClickAgain = (name,e) => {
+        alert("Hey "+ name);
+        console.log(e.target);
+    }
     return (  
         <div className="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}> Click me </button>
+            <button onClick={(e) => {
+                handleClickAgain('mario', e);
+            }}> Click me again</button>
         </div>
     );
 }
