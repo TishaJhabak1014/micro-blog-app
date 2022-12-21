4e24196b369a58e723106403201b13ebbc5f21f5
Tisha <iamtisha22@gmail.com>
2022-12-22 00:37:04 +1100

    updated routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import Navbar from './Navbar';
 import Home from './Home';
+import Create from './Create';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -9,7 +11,9 @@ function App() {
         <Navbar />
         <div className="content">
           <Routes>
-            <Route path="/" element={<Home />}>
+            <Route exact path="/" element={<Home />}>
+            </Route>
+            <Route exact path="/create" element={<Create />}>
             </Route>
           </Routes>
         </div>
