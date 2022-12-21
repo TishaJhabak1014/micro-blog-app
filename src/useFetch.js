f09cbceaf771bb12f48b5912b4324b9775dbec34
Tisha <iamtisha22@gmail.com>
2022-12-22 01:02:35 +1100

    added  fetch aborts

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,13 +1,18 @@
 import { useState , useEffect} from 'react';
 
+
+// we need to stop the fetch by useEffect when the componenet using it unmounts from the browser
+// we use clean-up and abort-controller
 const useFetch =(url)=> {
     const [data, setData] = useState(null);
     const [isPending, setIsPending] = useState(true);
     const [err, setErr] = useState(null);
 
     useEffect(() =>{
+        const abortCont = new AbortController();
+
         setTimeout(() => {
-            fetch(url)
+            fetch(url, {signal: abortCont.signal})
             .then(response => {
                 if(!response.ok){
                     throw Error("Could not fetch the data");
@@ -21,10 +26,20 @@ const useFetch =(url)=> {
                 setIsPending(false);
                 
             }).catch( error => {
-                setErr(error.message);
-                setIsPending(false);
+                if(error.name === 'AbortError'){
+                    console.log("fetch aborted");
+                }
+                else{
+                    setErr(error.message);
+                    setIsPending(false);
+                }
+                
             });
         }, 1000);
+
+        return () => {
+            abortCont.abort(); // aborts whatever fetch is associated with this
+        };
         
     }, [url]);
 
