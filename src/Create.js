fb3983733e1347c82010162733f64abc85ea3c23
Tisha <iamtisha22@gmail.com>
2022-12-22 02:11:50 +1100

    handling post requests

--- src/Create.js
+++ src/Create.js
@@ -4,12 +4,22 @@ const Create = () => {
     const [title, setTitle] = useState('give a title here');
     const [body, setBody] = useState('write your blog here');
     const [author, setAuthor] = useState('mario');
-
+    const [isPending, setIsPending] = useState(false);
 
     const handleSubmit = (e) =>{
         e.preventDefault();
         const blog = {title, body, author};
-        
+
+        setIsPending(true);
+
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(blog)
+        }).then(() => {
+            console.log("new blog added");
+            setIsPending(false);
+        });
     }
 
     return (  
@@ -25,7 +35,8 @@ const Create = () => {
                     <option value="mario">mario</option>
                     <option value="yoshi">yoshi</option>
                 </select>
-                <button>Add Blog</button>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Adding Blog...</button>}
             </form>
         </div>
     );
