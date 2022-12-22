cce512068e2eb1d6dbaf547969263642f9cf1a7f
Tisha <iamtisha22@gmail.com>
2022-12-23 01:01:32 +1100

    added styles

--- job-lister/src/App.js
+++ job-lister/src/App.js
@@ -9,12 +9,14 @@ function App() {
   return (
     <Router>
       <header>
+        <nav>
         <h1>JobHub</h1>
         {/* creates link that are handled within the browser */}
         {/* react-router-dom just swaps the content without sending any request to the browser */}
         {/* navlink has active class associated with it unlink link */}
         <NavLink to="/">Home</NavLink>
         <NavLink to="about">About</NavLink>
+        </nav>
       </header>
       <main> 
         {/* had to be rendered using urls as specified in path */}
