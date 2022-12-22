2716854ef05a30d7f5767cd753e7157e1dd041e6
Tisha <iamtisha22@gmail.com>
2022-12-23 00:38:18 +1100

    added basic route setup

--- job-lister/src/App.js
+++ job-lister/src/App.js
@@ -1,8 +1,20 @@
+import {BrowserRouter as Router, Routes, Route, Link} from 
+'react-router-dom';
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';;
+
 function App() {
   return (
-    <div className="App">
-      "Hello! Ninjas!"
-    </div>
+    <Router>
+      <main>
+        <Routes>
+          <Route path='/' element ={<Home/>}/>
+          <Route path='about' element ={<About/>}/>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
