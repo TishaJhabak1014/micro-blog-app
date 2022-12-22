df3fc0d699099db6d25cc44d9fc7f876a798f0ba
Tisha <iamtisha22@gmail.com>
2022-12-23 00:51:46 +1100

    added links and navlink

--- job-lister/src/App.js
+++ job-lister/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route, Link} from 
+import {BrowserRouter as Router, Routes, Route, Link, NavLink} from 
 'react-router-dom';
 
 // pages
@@ -8,7 +8,15 @@ import About from './pages/About';;
 function App() {
   return (
     <Router>
-      <main>
+      <header>
+        <h1>JobHub</h1>
+        {/* creates link that are handled within the browser */}
+        {/* react-router-dom just swaps the content without sending any request to the browser */}
+        <Link to="/">Home</Link>
+        <NavLink to="about">About</NavLink>
+      </header>
+      <main> 
+        {/* had to be rendered using urls as specified in path */}
         <Routes>
           <Route path='/' element ={<Home/>}/>
           <Route path='about' element ={<About/>}/>
