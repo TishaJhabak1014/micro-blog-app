4a16bbb8b6ced9ef3b1069acab77278c33554cf6
Tisha <iamtisha22@gmail.com>
2022-12-23 01:20:36 +1100

    updated app.js and routelayout

--- job-lister/src/App.js
+++ job-lister/src/App.js
@@ -1,32 +1,30 @@
-import {BrowserRouter as Router, Routes, Route, NavLink} from 
-'react-router-dom';
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
 
 // pages
-import Home from './pages/Home';
-import About from './pages/About';;
+import Home from './pages/Home'
+import About from './pages/About'
+
+// layouts
+import RootLayout from './layouts/RootLayout'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+)
 
 function App() {
   return (
-    <Router>
-      <header>
-        <nav>
-        <h1>JobHub</h1>
-        {/* creates link that are handled within the browser */}
-        {/* react-router-dom just swaps the content without sending any request to the browser */}
-        {/* navlink has active class associated with it unlink link */}
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main> 
-        {/* had to be rendered using urls as specified in path */}
-        <Routes>
-          <Route path='/' element ={<Home/>}/>
-          <Route path='about' element ={<About/>}/>
-        </Routes>
-      </main>
-    </Router>
+    <RouterProvider router={router} />
   );
 }
 
-export default App;
+export default App
