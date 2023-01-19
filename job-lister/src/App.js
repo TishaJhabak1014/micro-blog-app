0f7cd704f569c7dd4eef2333d25e7f8c5b6fea7c
Tisha <iamtisha22@gmail.com>
2023-01-20 01:08:06 +1100

    added form actions

--- job-lister/src/App.js
+++ job-lister/src/App.js
@@ -9,7 +9,7 @@ import {
 import Home from './pages/Home'
 import About from './pages/About'
 import Faq from './pages/help/Faq'
-import Contact from './pages/help/Contact'
+import Contact, { contactAction } from './pages/help/Contact'
 import NotFound from './pages/NotFound'
 import Careers from './pages/careers/Careers'
 import { careersLoader } from './pages/careers/Careers'
@@ -28,7 +28,7 @@ const router = createBrowserRouter(
       <Route path="about" element={<About />} />
       <Route path="help" element={<HelpLayout />}>
         <Route path="faq" element={<Faq />} />
-        <Route path="contact" element={<Contact/>} />
+        <Route path="contact" element={<Contact/>} action={contactAction} />
       </Route>
 
       {/* loaders are used to load data to a component before it renders, we no longer need a ueEffect hook*/}
